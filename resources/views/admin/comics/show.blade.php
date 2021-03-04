6f7313b4df93efb41f8da8111bdc21efc56f7712
Rocco Orefice <rocco.orefice92@gmail.com>
2021-03-04 19:28:56 +0100

    details comic view

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,10 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
-  <div class="card-body">
-    <h4 class="card-title">{{ $comic->title }}</h4>
-    <p class="card-text">{{ $comic->slug }}</p>
-  </div>
+
+<div class="details_comic container">
+
+  <img src="{{ asset('storage/'  . $comic->cover) }}" alt="" srcset="">
+  <h4 class="card-title">{{ $comic->title }}</h4>
+  <table>
+    <thead>
+      <tr>
+        <th>U.S Price: {{ $comic->price }}</th>
+        <th>
+          @if ($comic->available == "yes")
+              AVAILABLE
+          @else
+              NOT AVAILABLE
+          @endif
+        </th>
+        <th>Check Availability <i class="fas fa-caret-down"></i></th>
+      </tr>
+    </thead>
+  </table>
+  <p class="w-50">{{ $comic->description }}</p>
+    
+
+  
 </div>
+
 @endsection
