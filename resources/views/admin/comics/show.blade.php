a3838ae8e36d92b035190eaadb9f26d919dc4a2d
Rocco Orefice <rocco.orefice92@gmail.com>
2021-03-05 13:25:47 +0100

    section must reads complete

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -2,29 +2,78 @@
 
 @section('content')
 
-<div class="details_comic container">
+<div class="details_comic">
+  <div class="container">
+    <img src="{{ asset('storage/'  . $comic->cover) }}" alt="" srcset="">
+    <h4 class="card-title">{{ $comic->title }}</h4>
+    <table class="table_green">
+      <thead>
+        <tr>
+          <th>U.S Price: {{ $comic->price }}</th>
+          <th>
+            @if ($comic->available == "yes")
+                AVAILABLE
+            @else
+                NOT AVAILABLE
+            @endif
+          </th>
+          <th>Check Availability <i class="fas fa-caret-down"></i></th>
+        </tr>
+      </thead>
+    </table>
+    <p class="w-50">{{ $comic->description }}</p>
+  </div>
 
-  <img src="{{ asset('storage/'  . $comic->cover) }}" alt="" srcset="">
-  <h4 class="card-title">{{ $comic->title }}</h4>
-  <table>
-    <thead>
-      <tr>
-        <th>U.S Price: {{ $comic->price }}</th>
-        <th>
-          @if ($comic->available == "yes")
-              AVAILABLE
-          @else
-              NOT AVAILABLE
-          @endif
-        </th>
-        <th>Check Availability <i class="fas fa-caret-down"></i></th>
-      </tr>
-    </thead>
-  </table>
-  <p class="w-50">{{ $comic->description }}</p>
-    
 
-  
+  <div class="more_details">
+    <div class="container">
+
+      <div class="talent">
+        <h5>Talent</h5>
+        <table class="table">
+          <tr>
+            <td class="col-tit">Art by:</td>
+            <td>{{ $comic->artist }}</td>
+          </tr>
+          <tr>
+            <td class="col-tit">Written by:</td>
+            <td>{{ $comic->author }}</td>
+          </tr>
+        </table>
+      </div>
+      <div class="specs">
+        <h5>Specs</h5>
+        <table class="table">
+          <tr>
+            <td class="col-tit">Series:</td>
+            <td>{{ $comic->title }}</td>
+          </tr>
+          <tr>
+            <td class="col-tit">U.S price:</td>
+            <td>{{ $comic->price }}</td>
+          </tr>
+          <tr>
+            <td class="col-tit">On Sale Date:</td>
+            <td>{{ $comic->created_at }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <div class="blu_ban">
+    <ul class="list-unstyled">
+      <a href=""><li>DIGITAL COMICS</li></a>
+      <a href=""><li>DC MERCHANDISE</li></a>
+      <a href=""><li>PRINT SUBSCRIPTIONS</li></a>
+      <a href=""><li>COMIC SHOP LOCATOR</li></a>
+    </ul>
+  </div>
 </div>
+  
+
+
+ 
+
 
 @endsection
--- resources/views/guests/comics/index.blade.php
+++ resources/views/guests/comics/index.blade.php
@@ -17,15 +17,41 @@
                               @else
                               NOT AVAILABLE
                               @endif
-                              
                         </p>
                   </div>
-
             @endforeach
       </div>
-          
+</div>
+
+<div class="must_read container">
+      <h2 class="mr_title">MUST READS</h2>
+
+
+      <div class="mr_item">
+            <img src="{{ asset("img/mustread_batman.jpg") }}" alt="" srcset="">
+            <h4>GET THE INSIDE TRACK ON ALL THINGS DC</h4>
+            <h2>DC CONNECT</h2>
+            <p>The monthly, downloadable catalog of DC's comics, graphic novels, collectibles and more!</p>
+      </div>
+
+      <div class="mr_item">
+            <img src="{{ asset("img/mustread_sandman.jpg") }}" alt="" srcset="">
+            <h4>FROM THE MIND OF NEIL GAIMAN</h4>
+            <h2>THE SANDMAN UNIVERSE</h2>
+            <p>Four new series set in the world of the Vertigo classic.</p>
+      </div>
+
+      <div class="mr_item">
+            <img src="{{ asset("img/mustread_wonder.jpg") }}" alt="" srcset="">
+            <h4>THESE KIDS ARE ALL FIGHT!</h4>
+            <h2>WONDER COMICS</h2>
+            <p>Brian Michael Bendis shines the spotlight on DC’s youngest heroes!</p>
+      </div>
 
 </div>
+
+
+
 
 
 
